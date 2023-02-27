ddeef22cef6dc240d7211bceb8ab0706357e4b1a
Lyciab <63170852+Lyciab@users.noreply.github.com>
2023-02-27 19:29:18 +0000

    updated module-info to support log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module uk.ac.soton.comp2211.team_43_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
 
     opens uk.ac.soton.comp2211.team_43_project to javafx.fxml;
